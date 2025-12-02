3280d3db0f856f9fba8f8438bbe0aea0cf7099a5
jp60302 <jpearlmutter@tlit60302.com>
2025-12-02 10:30:07 -0700

    feat: add PowerShell script for Firefox kiosk mode
    Introduce a new PowerShell script `firefox-kiosk.ps1` to launch
    Firefox in kiosk mode with a predefined URL. This facilitates
    a simple setup for running a browser in a locked-down mode,
    suitable for public or restricted access scenarios. The script
    ensures Firefox restarts automatically upon closure, maintaining
    consistent access without manual intervention.

---
+++ firefox-kiosk.ps1
@@ -0,0 +1,8 @@
+# Variables
+$WebsiteUrl = "https://insperity.myisolved.com/UserLogin.aspx?ReturnUrl=%2fdefault.aspx"
+
+# Launch Firefox and relaunch when it's closed
+while ($true) {
+    Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -Wait -ArgumentList "-kiosk $WebsiteUrl"
+    Write-Host "Did this appear before closing FireFox?"
+}
